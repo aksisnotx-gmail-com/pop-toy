8c47d0f33481a52731dc8fe860ba7e81ff2ba75c
xxl <3578144921@qq.com>
2024-12-15 04:15:45 +0800

    feat(手办定制增加材质 + 工艺筛选):

--- “POP TOY”手办销售平台springboot20g2h564/springboot20g2h564/src/main/java/com/service/impl/ShoubandingzhiServiceImpl.java
+++ “POP TOY”手办销售平台springboot20g2h564/springboot20g2h564/src/main/java/com/service/impl/ShoubandingzhiServiceImpl.java
@@ -1,8 +1,13 @@
 package com.service.impl;
 
+import com.entity.ShejifanganEntity;
+import com.service.ShejifanganService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
@@ -18,9 +23,13 @@ import com.service.ShoubandingzhiService;
 import com.entity.vo.ShoubandingzhiVO;
 import com.entity.view.ShoubandingzhiView;
 
+import javax.annotation.Resource;
+
 @Service("shoubandingzhiService")
+@RequiredArgsConstructor
 public class ShoubandingzhiServiceImpl extends ServiceImpl<ShoubandingzhiDao, ShoubandingzhiEntity> implements ShoubandingzhiService {
-	
+
+	private final ShejifanganService shejifanganService;
 	
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -33,11 +42,43 @@ public class ShoubandingzhiServiceImpl extends ServiceImpl<ShoubandingzhiDao, Sh
     
     @Override
 	public PageUtils queryPage(Map<String, Object> params, Wrapper<ShoubandingzhiEntity> wrapper) {
-		  Page<ShoubandingzhiView> page =new Query<ShoubandingzhiView>(params).getPage();
-	        page.setRecords(baseMapper.selectListView(page,wrapper));
-	    	PageUtils pageUtil = new PageUtils(page);
-	    	return pageUtil;
- 	}
+		Page<ShoubandingzhiView> page = new Query<ShoubandingzhiView>(params).getPage();
+		page.setRecords(baseMapper.selectListView(page, wrapper));
+
+		Object material = params.get("material");
+		Object crafts = params.get("crafts");
+		if (Objects.nonNull(material) || Objects.nonNull(crafts)) {
+			List<ShoubandingzhiView> collect = page.getRecords().stream().
+					filter(t -> {
+						List<ShejifanganEntity> ls = shejifanganService.selectList(new EntityWrapper<ShejifanganEntity>().eq("gonghao", t.getGonghao()));
+
+                        if (ls.isEmpty()) {
+							return false;
+                        }
+
+                        if (Objects.nonNull(material) && Objects.nonNull(crafts)) {
+                            return ls.stream().anyMatch(t1 -> Objects.equals(t1.getCaizhi(),material) &&
+									Objects.equals(t1.getGongyijianyi(),crafts));
+                        }
+
+						if (Objects.nonNull(material)) {
+							return ls.stream().anyMatch(t1 -> Objects.equals(t1.getCaizhi(),material));
+						}
+
+						if (Objects.nonNull(crafts)) {
+							return ls.stream().anyMatch(t1 -> Objects.equals(t1.getGongyijianyi(),crafts));
+						}
+
+						return true;
+					}).
+					collect(Collectors.toList());
+
+			page.setRecords(collect);
+			page.setTotal(collect.size());
+		}
+		PageUtils pageUtil = new PageUtils(page);
+		return pageUtil;
+	}
 
     
     @Override
